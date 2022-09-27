1f7bc02028e9151997e63057959b8bb0f4f75829
Sahid Rahman <sahidrahman404@gmail.com>
2022-09-27 07:10:03 +0700

    swap key map

--- skeletyl.h
+++ skeletyl.h
@@ -31,10 +31,10 @@
   {   k00,   k01,   k02,   k03,   k04 },                \
   {   k10,   k11,   k12,   k13,   k14 },                \
   {   k20,   k21,   k22,   k23,   k24 },                \
-  {   KC_NO, KC_NO, k30,   k33,   k32 },                \
+  {   KC_NO, KC_NO, k32,   k33,   k30 },                \
   {   k40,   k41,   k42,   k43,   k44 },                \
   {   k50,   k51,   k52,   k53,   k54 },                \
   {   k60,   k61,   k62,   k63,   k64 },                \
-  {   KC_NO, KC_NO, k70,   k73,   k72 },                \
+  {   KC_NO, KC_NO, k72,   k73,   k70 },                \
 }
 // clang-format on
